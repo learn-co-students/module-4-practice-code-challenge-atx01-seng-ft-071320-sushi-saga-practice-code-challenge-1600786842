1312f7f3759bb1c04719afbf896e4d9e6eec9034
Gabe-Corral <78gabe.corral@gmail.com>
2020-09-22 12:10:53 -0500

    submission for deadline

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,38 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushi: [],
+    nextRound: 0,
+    nextRound2: 4,
+    notEated: false,
+    money: 100,
+    sushiEated: []
+  }
+
+  componentDidMount = () => {
+    fetch(API).then(res => res.json())
+    .then(sushi => this.setState({ sushi }))
+  }
+
+  nextSushi = () => {
+    this.setState({ nextRound: this.state.nextRound + 4 })
+    this.setState({ nextRound2: this.state.nextRound2 + 4 })
+  }
+
+  eatedSushi = (e) => {
+    this.setState({ notEated: !this.state.notEated })
+    this.setState({ sushiEated: [...this.state.sushiEated, "plate"] })
+    //this.setState({ money: this.state.money })
+    //console.log(this.state.sushiEated)
+  }
+
   render() {
+    const nSushi = this.state.sushi.slice(this.state.nextRound, this.state.nextRound2)
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer  eatedSushi={this.eatedSushi} eat={this.state.notEated} nextSushi={this.nextSushi} sushi={nSushi} />
+        <Table money={this.state.money} plate={this.state.sushiEated} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,15 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          props.sushi.map(sushi => <Sushi sushi={sushi} key={sushi.id} eatedSushi={props.eatedSushi} eat={props.eat} />)
         }
-        <MoreButton />
+        <MoreButton nextSushi={props.nextSushi}/>
       </div>
     </Fragment>
   )
